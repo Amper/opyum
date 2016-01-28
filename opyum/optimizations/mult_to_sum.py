4a24136d4cdb6f8bef81ba9917906ab3d8b1ed42
Alexander Marshalov <_@marshalov.org>
2016-01-28 07:26:56 +0600

    ...

--- opyum/optimizations/mult_to_sum.py
+++ opyum/optimizations/mult_to_sum.py
@@ -27,15 +27,31 @@ class MultToSum(ASTOptimization):
 
     def _is_numeric_mult(self, node: ast.BinOp) -> bool:
         if isinstance(node.op, ast.Mult):
-            if  isinstance(node.left, (ast.Name, ast.Num)) \
+            if  isinstance(node.left , (ast.Name, ast.Num)) \
             and isinstance(node.right, ast.Num):
+                return True
+            if  isinstance(node.right, (ast.Name, ast.Num)) \
+            and isinstance(node.left , ast.Num):
                 return True
         return False
 
     def visit_BinOp(self, node: ast.BinOp) -> ast.BinOp:
         node = self.generic_visit(node)
         if self._is_numeric_mult(node):
-            if node.right.n == 2:
-                node.op    = ast.copy_location(ast.Add(), node.op)
-                node.right = copy(node.left)
+            if isinstance(node.right, ast.Num):
+                if node.right.n == 2:
+                    node.op    = ast.copy_location(ast.Add(), node.op)
+                    node.right = copy(node.left)
+                elif node.right.n == 1:
+                    node       = node.left
+                elif node.right.n == 0:
+                    node       = ast.copy_location(ast.Num(n = 0), node)
+            elif isinstance(node.left , ast.Num):
+                if node.left.n == 2:
+                    node.op    = ast.copy_location(ast.Add(), node.op)
+                    node.left  = copy(node.right)
+                elif node.left.n == 1:
+                    node       = node.right
+                elif node.left.n == 0:
+                    node       = ast.copy_location(ast.Num(n = 0), node)
         return node
--- tests/tests.py
+++ tests/tests.py
@@ -5,11 +5,31 @@ from opyum import *
 
 class TestApp(unittest.TestCase):
 
-    def test_decorator(self):
-    	@optimize
-        def test_func(a, b):
-            return 1 + 2 + 3 * 4 + b + c
-        self.assertTrue(True)
+    def test_mult_to_sum_1(self):
+        src = ( "n = n * 0\n"
+                "n = 0 * n\n"
+                "n = n * 1\n"
+                "n = 1 * n\n"
+                "n = n * 2\n"
+                "n = 2 * n\n"
+                "n = n * 3\n"
+                "n = 3 * n"
+              )
+        new_src = get_source\
+                    ( value         = src
+                    , optimized     = True
+                    , optimizations = [ast_optimizations['MultToSum']]
+                    )
+        self.assertTrue(new_src == ( "n = 0\n"
+                                     "n = 0\n"
+                                     "n = n\n"
+                                     "n = n\n"
+                                     "n = (n + n)\n"
+                                     "n = (n + n)\n"
+                                     "n = (n * 3)\n"
+                                     "n = (3 * n)"
+                                   )
+                       )
 
 
 if __name__ == '__main__':
