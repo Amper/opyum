9032f492652defd77a4651766ace1d34fbba4591
Alexander Marshalov <_@marshalov.org>
2016-01-28 22:20:36 +0600

    Fix pow_to_mult and add test for pow_to_mult

--- opyum/optimizations/pow_to_mult.py
+++ opyum/optimizations/pow_to_mult.py
@@ -13,7 +13,7 @@ class PowToMult(ASTOptimization):
 
     """
 
-    MAX_DEGREE = 5
+    MAX_DEGREE = 3
 
     @property
     def name(self) -> str:
@@ -29,9 +29,16 @@ class PowToMult(ASTOptimization):
 
     def _is_numeric_pow(self, node: ast.BinOp) -> bool:
         if isinstance(node.op, ast.Pow):
-            if  isinstance(node.left, (ast.Name, ast.Num)) \
-            and isinstance(node.right, ast.Num):
-                degree = node.right.n
+            left, right = node.left, node.right
+            if isinstance(left, (ast.Name, ast.Num)):
+                if isinstance(right, ast.Num):
+                    degree = right.n
+                elif isinstance(right, ast.UnaryOp)\
+                and  isinstance(right.op, (ast.USub, ast.UAdd))\
+                and  isinstance(right.operand, ast.Num):
+                    degree = right.operand.n
+                else:
+                    return False
                 if isinstance(degree, float):
                     degree = int(degree) if degree.is_integer() else degree
                 return isinstance(degree, int)
@@ -40,13 +47,30 @@ class PowToMult(ASTOptimization):
     def visit_BinOp(self, node: ast.BinOp):
         node = self.generic_visit(node)
         if self._is_numeric_pow(node):
-            left, right  = node.left, node.right
-            if right.n == 0:
+            left, right = node.left, node.right
+            degree = (    right.n         if isinstance(right, ast.Num) 
+                    else -right.operand.n if isinstance(right.op, ast.USub) 
+                    else  right.operand.n )
+            if abs(degree) == 0:
                 node = ast.copy_location(ast.Num(n = 1), node)
-            elif right.n == 1:
+            elif abs(degree) == 1:
                 node = node.left
-            elif 2 <= right.n <= self.MAX_DEGREE:
-                for num in range(1, node.right.n):
-                    new_node = ast.BinOp(left = left, op = ast.Mult(), right = copy(node.left))
-                    left = node = ast.copy_location(new_node, node)
+            elif 2 <= abs(degree) <= self.MAX_DEGREE:
+                for _ in range(1, abs(degree)):
+                    new_node = ast.BinOp\
+                                ( left = left
+                                , op = ast.Mult()
+                                , right = copy(node.left)
+                                )
+                    left = new_node = ast.copy_location(new_node, node)
+                node = new_node
+            else:
+                return node
+            if degree < 0:
+                new_node = ast.BinOp\
+                                ( left  = ast.Num(n = 1)
+                                , op    = ast.Div()
+                                , right = node 
+                                )
+                node = ast.copy_location(new_node, node)
         return node
--- tests/tests.py
+++ tests/tests.py
@@ -32,6 +32,39 @@ class TestApp(unittest.TestCase):
                        )
 
 
+    def test_pow_to_mult(self):
+        src = ( "n = n ** (-4)\n"
+                "n = n ** (-3)\n"
+                "n = n ** (-2)\n"
+                "n = n ** (-1.5)\n"
+                "n = n ** (-1)\n"
+                "n = n ** 0\n"
+                "n = n ** 1\n"
+                "n = n ** 2\n"
+                "n = n ** (+2)\n"
+                "n = n ** 3\n"
+                "n = n ** 4\n"
+              )
+        new_src = get_source\
+                    ( value         = src
+                    , optimized     = True
+                    , optimizations = [ast_optimizations['PowToMult']]
+                    )
+        self.assertTrue(new_src == ( "n = (n ** (- 4))\n"
+                                     "n = (1 / ((n * n) * n))\n"
+                                     "n = (1 / (n * n))\n"
+                                     "n = (n ** (- 1.5))\n"
+                                     "n = (1 / n)\n"
+                                     "n = 1\n"
+                                     "n = n\n"
+                                     "n = (n * n)\n"
+                                     "n = (n * n)\n"
+                                     "n = ((n * n) * n)\n"
+                                     "n = (n ** 4)"
+                                   )
+                       )
+
+
 if __name__ == '__main__':
     unittest.main()
 
